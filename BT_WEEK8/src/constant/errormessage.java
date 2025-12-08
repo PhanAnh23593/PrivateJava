7dbbd523bdc0226ead93a4494fb52fa8695ab212
Phan the anh <anh988998@gmail.com>
2025-12-08 22:09:13 +0700

    fix

--- BT_WEEK8/src/constant/errormessage.java
+++
@@ -1,6 +0,0 @@
-package constant;
-
-public class errormessage {
-    public static String IDINVALLID = " Không tìm thấy ID";
-    public static String INVALID_CHONCE = " Lưa chọn không hợp lệ : ";
-}
--- BT_WEEK8/src/constant/successmessage.java
+++
@@ -1,6 +0,0 @@
-package constant;
-
-public class successmessage {
-    public static String ADDSUCCESS = "Thêm Sách thành công vào thư viện ";
-    public static String SUCCESSDELTE = " FINISH DELTETE BOOK ";
-}
